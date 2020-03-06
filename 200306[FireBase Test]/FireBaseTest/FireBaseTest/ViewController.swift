b3b81103234464801e7e78d0c13092bd6b4b43e9
eunyuni <eunyuuni@gmail.com>
2020-03-06 20:50:26 +0900

    FireBase TableView Test

---
+++ 200306[FireBase Test]/FireBaseTest/FireBaseTest/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  FireBaseTest
+//
+//  Created by 은영김 on 2020/03/05.
+//  Copyright © 2020 eunyuni. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+  
+  var ref: DatabaseReference!
+  
+  @IBOutlet weak var tableView: UITableView!
+  var todos = [String]()
+  
+  
+  @IBAction func addTodo(_ sender: Any) {
+    let dialog = UIAlertController(title: "할일을 추가해용!", message: nil, preferredStyle: .alert)
+    dialog.addTextField()
+    let okAction = UIAlertAction(title: "OK", style: .default) { (action : UIAlertAction) in
+      if let newTodo = dialog.textFields?[0].text {
+        print("입력할 할일 : \(newTodo)")
+        self.todos.append(newTodo)
+        self.tableView.reloadData()
+        
+        self.ref = Database.database().reference()
+        let itemRef = self.ref.child("list")
+        itemRef.setValue(self.todos)
+      }
+      
+    }
+    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+    dialog.addAction(okAction)
+    dialog.addAction(cancelAction)
+    self.show(dialog, sender: nil)
+  }
+  
+  private func okAction() {
+    
+  }
+  
+  
+}
+
+extension ViewController: UITableViewDataSource {
+  
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return self.todos.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
+    cell.textLabel?.text = self.todos[indexPath.row]
+    return cell
+  }
+}
+
