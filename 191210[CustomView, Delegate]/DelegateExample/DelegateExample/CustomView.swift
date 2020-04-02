b20b178cb43c860ca7e24e0fdca762e60f1425fd
eunyuni <eunyuuni@gmail.com>
2020-04-02 21:19:41 +0900

    Add: Delegate 강의 Add: TextFieldDelegate 문제풀이

--- 191210[CustomView, Delegate]/DelegateExample/DelegateExample/CustomView.swift
+++ 191210[CustomView, Delegate]/DelegateExample/DelegateExample/CustomView.swift
@@ -8,14 +8,64 @@
 
 import UIKit
 
-class CustomView: UIView {
+// 1. 프로토콜 정의
+protocol CustomViewDelegate: class{
+  func colorForBackground(_ newColor: UIColor?) -> UIColor
+}
+// 커스텀뷰에서 지정된것만 사용이 가능했는데 델리게이트를 해줌으로써 자유도가 생김
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+
+class CoustomView: UIView {
+  
+  // 2. delegate 프로퍼티 선언
+  weak var delegate: CustomViewDelegate?
+  // (프로토콜에서 클래스를 상속받지 않으면) weak로 만들면 오류발생 / 프로토콜이 클래스 바운드여야한다 weak는 참조와관련되어있는데 참조는 클래스와관련있고 이것을 프로토콜에 클래스라고 한정지어줘야한다.
+  // weak는 class에서 사용하기때문에 protocol도 class를 상속해줘야함
+  
+  override var backgroundColor: UIColor? {
+    // override한 프로퍼티는 연산으로 사용해야함 저장이불가
+    get { super.backgroundColor }
+    set {
+      
+      // 3. delegate 메서드호출
+      let color = delegate?.colorForBackground(newValue)
+      // 델리게이트가 누가될진모르지만 델리한테물어봄 이걸 받아서 처리해줄 객체가 있다면 그놈이 받아서 처리해줘(프로토콜을 통해) 컬러포백그라운드 함수를 수행시킬거니까 이걸 구현해서 컬러를 돌려줘라 라는의미..ㅎ
+      let newColor = color ?? newValue ?? .gray
+      super.backgroundColor = newColor
+      print("새로변경될 색은:", backgroundColor!)
+      
+// super.backgroundColor = newValue
+// print("새로 변경될 색은: ", newValue)
+      
+      // super.backgroundColor = newValue ?? .black
+      
+      // if newValue == .red {
+      // super.backgroundColor = .blue
+      // }
+      // 고정값을 주는것.
+      
+      // if newValue == .red {
+      // super.backgroundColor = .blue
+      // print("새로 변경될 색은: ", newValue)
+      // } else if newValue == nil {
+      // super.backgroundColor = .black
+      // print("새로 변경될 색은: ", newValue)
+      // } else {
+      // super.backgroundColor = newValue
+      // print("새로 변경될 색은: ", newValue)
+      // }
+      
+      // switch newValue {
+      // case UIColor.red:
+      // super.backgroundColor = .blue
+      // case nil:
+      // super.backgroundColor = .black
+      // default:
+      // super.backgroundColor = newValue
+      // }
+      
     }
-    */
-
+  }
+  // 나만의 커스텀뷰를 만들어서 사용
+  // 커스텀뷰를 상속받으면 항상 프린트함
 }
--- 191210[CustomView, Delegate]/DelegateExample/DelegateExample/ViewController.swift
+++ 191210[CustomView, Delegate]/DelegateExample/DelegateExample/ViewController.swift
@@ -9,12 +9,116 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  let myView = CoustomView()
+  let myView1 = UIView()
+  let textField = UITextField()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    
+//    myView.frame = CGRect(x: 40, y: 80, width: 160, height: 160)
+    
+    myView.backgroundColor = .systemBlue
+    myView.backgroundColor = .red // -> blue
+    myView.backgroundColor = nil // -> gray
+    
+    // 2. delegate = self
+    myView.delegate = self
+    myView.backgroundColor = .red
+    
+    myView1.backgroundColor = .yellow
+    textField.borderStyle = .roundedRect
+    setUI()
+    
+    textField.delegate = self
   }
+  
+  private func setUI() {
+    
+    view.addSubview(myView)
+    view.addSubview(myView1)
+    view.addSubview(textField)
+    
+    let guide = view.safeAreaLayoutGuide
+    
+    myView.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      myView.topAnchor.constraint(equalTo: guide.topAnchor, constant: 40),
+      myView.centerXAnchor.constraint(equalTo: guide.centerXAnchor),
+      myView.widthAnchor.constraint(equalToConstant: 300),
+      myView.heightAnchor.constraint(equalToConstant: 100)
+    ])
+    
+    myView1.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      myView1.topAnchor.constraint(equalTo: myView.bottomAnchor, constant: 20),
+      myView1.centerXAnchor.constraint(equalTo: guide.centerXAnchor),
+      myView1.widthAnchor.constraint(equalToConstant: 340),
+      myView1.heightAnchor.constraint(equalToConstant: 100)
+    ])
+    
+    textField.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      textField.topAnchor.constraint(equalTo: myView1.bottomAnchor, constant: 40),
+      textField.centerXAnchor.constraint(equalTo: guide.centerXAnchor),
+      textField.widthAnchor.constraint(equalToConstant: 340),
+      textField.heightAnchor.constraint(equalToConstant: 50)
+    ])
 
-
+  }
+  
 }
 
+// MARK: -CustomViewDelegate
+
+// 1. Delegate 프로토콜 채택
+extension ViewController: CustomViewDelegate {
+  func colorForBackground(_ newColor: UIColor?) -> UIColor {
+    guard let color = newColor else {
+      return .black
+    }
+    return color == .red ? .blue : color
+  }
+}
+
+// MARK: -UITextFieldDelegate
+
+extension ViewController: UITextFieldDelegate {
+  
+  // return눌렀을때 return을 할건지 말건지
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    // 키보드 내리기
+    textField.resignFirstResponder()
+    return true
+  }
+  
+  // textfield종료시 호출
+  func textFieldDidEndEditing(_ textField: UITextField) {
+//    if textField.text == "red" {
+//      myView1.backgroundColor = .red
+//    } else if textField.text == "orang" {
+//      myView1.backgroundColor = .orange
+//    } else {
+//      myView1.backgroundColor = .gray
+//    }
+    guard let text = textField.text else { return }
+    switch text {
+    case "red": myView1.backgroundColor = .red
+    case "blue": myView1.backgroundColor = .orange
+    case "black": myView1.backgroundColor = .black
+    default: myView1.backgroundColor = .gray
+    }
+  }
+  
+  // 내가원하는 글자를 입력못하도록 할수있음
+//  func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//
+//
+//    // a는 입력못하도록
+//    return string == "a" ? false : true
+//  }
+  
+  
+}
